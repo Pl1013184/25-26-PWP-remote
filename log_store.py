7dcfb2bb98c28f0ada860cfbf0b24b87561594ca
Pl1013184 <pl1013184@gmail.com>
2026-05-13 10:20:58 -0400

    new repo commit -note! dont rm log

---
+++ log_store.py
@@ -0,0 +1,25 @@
+
+from datetime import datetime as dt
+from datetime import timezone
+utc_dt=dt.now(timezone.utc)
+l_dt=utc_dt.astimezone()
+partial_l=""
+try:
+    openlog=open("log.txt","x")
+    openlog.close()
+    #openlog = open("log.txt","a+")
+except:
+    openlog=open("log.txt","w")
+    openlog.write("")
+    openlog.close()
+def log_sto(info):
+    partial_log=""
+    openlog = open("log.txt","a+")
+    openlog.write(info+"["+l_dt.now().strftime("%H:%M:%S")+"]"+"<br>\n")
+    openlog.seek(0)
+    for i in openlog.readlines()[-32:]:
+        partial_log+=i.replace("\n","")
+    return partial_log
+def gimmefull():
+    read=open("log.txt","r")
+    return read.read()
